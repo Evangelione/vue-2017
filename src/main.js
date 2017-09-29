1489a89cd9b705e9ec0205a76af8d66af66646f3
cc <102075776@qq.com>
2017-09-29 09:45:50 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// 引入axios
+// import api from './api/index.js'
+import axios from 'axios'
+// 绑定到vue全局使用
+Vue.prototype.$axios = axios
+
+import utils from './utils'
+Vue.prototype.$utils = utils
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
